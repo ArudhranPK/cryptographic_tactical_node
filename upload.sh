f21d855cbe61f00f9106e8a255ad653c85d44d52
ArudhranPK <github@arudhran.in>
2026-08-05 01:29:47 +0530

    init

--- upload.sh
+++ upload.sh
@@ -1,4 +1,14 @@
-read -p "Enter commit comment:" comment 
+#!/bin/bash
+
+# Adding a space after the colon makes it easier to read when typing
+read -p "Enter commit comment: " comment 
+
+# Exit the script if the user just presses Enter without typing a message
+if [ -z "$comment" ]; then
+  echo "Error: Commit message cannot be empty."
+  exit 1
+fi
+
 git add .
 git commit -m "$comment"
 git push origin main
